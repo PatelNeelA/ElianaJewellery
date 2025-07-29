6e98ca4603a2196855f2e712fbf0243f28c45860
Patel Neel <161935098+PatelNeelA@users.noreply.github.com>
2025-07-29 09:54:29 +0530

    Update Login.jsx

--- Eliana-jewellery-main/src/Components/Login/Login.jsx
+++ Eliana-jewellery-main/src/Components/Login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://elianajewellery-backend.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
